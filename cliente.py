5fcd660491211908aa64eec202f7736517f55305
mibasFerraz <lucasllff@gmail.com>
2022-12-21 08:38:59 -0300

    adicionando cliente

---
+++ cliente.py
@@ -0,0 +1,35 @@
+import socket
+from time import sleep
+
+HOST = 'localhost'
+PORT = 8082  
+
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+destino = (HOST, PORT)
+
+tcp.connect(destino)
+
+print('Cliente um ligado.\n')
+
+while True:
+       
+    inicio = input("O que voce deseja fazer?(ligar/desligar/sair)\n")
+    tcp.send(str(inicio).encode())
+    if inicio == 'sair':
+        break
+    
+    if inicio == 'ligar':
+        objeto = input("O que voce deseja ligar?(projetor/ lampada1/ lampada2/ arcondicionado/ sensorDePresenca/ sensorDeFumaca/ sensorDePorta/ sensorDeJanela)\n")    
+        tcp.send(str(objeto).encode())
+        
+    if inicio == 'desligar':
+        objeto = input("O que voce deseja desligar?(projetor/ lampada1/ lampada2/ arcondicionado/ sensorDePresenca/ sensorDeFumaca/ sensorDePorta/ sensorDeJanela)\n")        
+        tcp.send(str(objeto).encode())
+    
+    sleep(10)
+    frase = tcp.recv(1024)
+    print(frase)
+    
+print('Volte sempre.')
+tcp.close()
