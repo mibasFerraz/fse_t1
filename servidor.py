94ff40a215e9b79742f7b87dad628f5df8267a02
mibasFerraz <lucasllff@gmail.com>
2022-12-21 08:40:19 -0300

    correcao

--- servidor.py
+++ servidor.py
@@ -1,120 +1,35 @@
 import socket
-#from gpiozero import LED
-#from gpiozero import Buzzer
 from time import sleep
-from signal import pause
-import adafruit_dht
-
-def printa(frase):
-    print(frase)
-    #tcp.send(str(frase).encode())
-
-def ligaLed(porta):
-    led = LED(porta)
-    led.on()
-
-def desligaLed(porta):
-    led = LED(porta)
-    led.off()
-
-def ligaAlarme(porta):
-    led = LED(porta)
-    led.on()
-            
-def desligaAlarme(porta):
-    led = LED(porta)
-    led.off
 
 HOST = 'localhost'
-PORT = 8082
+PORT = 8082  
 
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-origem = (HOST, PORT)
+destino = (HOST, PORT)
 
-tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-tcp.bind(origem)
+tcp.connect(destino)
 
-tcp.listen(1)
-
-print('Servidor central ligado.')
+print('Cliente um ligado.\n')
 
 while True:
-
-    conexao, cliente = tcp.accept()
-
-    print('\nConexão realizada por:', cliente)
-
-    while True:
-
-        instrucao = conexao.recv(1024)
-        estado = conexao.recv(1024)
-
-        if not instrucao:
-
-            break
-
-        objeto = estado.decode()
-        inicio = instrucao.decode()
-                
-        if inicio == 'ligar':
-            if objeto == 'projetor':
-                #ligaLed(25)
-                printa('Projetor ligado.')
-                
-            if objeto == "lampada1":
-                ligaLed(18)
-                print("Lampada 1 ligada.")
-                
-            if objeto == 'lampada2':
-                ligaLed(23)
-                print("Lampada 2 ligada.")
-                        
-            if objeto == "arcondicionado":
-                ligaLed(24)
-                print("Arcondicionado ligado.")
-            
-            if objeto == "sensorDePresenca":
-                print("sensorDePresenca")
-            
-            if objeto == "sensorDeFumaca":
-                ligaLed(1)
-                ligaAlarme(8)
-            
-            if objeto == "sensorDePorta":
-                print("sensorDePorta")
-            
-            if objeto == "sensorDeJanela":
-                print("sensorDeJanela")
-
-
-        if inicio == "desligar":        
-            if objeto == "projetor":
-                desligaLed(25)
-                print("Projetor desligado.")
-                
-            if objeto == "lampada1":
-                desligaLed(18)
-                            
-            if objeto == 'lampada2':
-                desligaLed(23)  
-                        
-            if objeto == "arcondicionado":
-                desligaLed(24)
-                
-            if objeto == "sensorDePresenca":
-                print("sensorDePresenca")
-                
-            if objeto == "sensorDeFumaca":
-                desligaLed(1)
-                desligaAlarme(8)
-                
-            if objeto == "sensorDePorta":
-                print("sensorDePorta")    
-            
-            if objeto == "sensorDeJanela":
-                print("sensorDeJanela")     
+       
+    inicio = input("O que voce deseja fazer?(ligar/desligar/sair)\n")
+    tcp.send(str(inicio).encode())
+    if inicio == 'sair':
+        break
+    
+    if inicio == 'ligar':
+        objeto = input("O que voce deseja ligar?(projetor/ lampada1/ lampada2/ arcondicionado/ sensorDePresenca/ sensorDeFumaca/ sensorDePorta/ sensorDeJanela)\n")    
+        tcp.send(str(objeto).encode())
         
-    print('Finalizando conexao do cliente', cliente)
-
-    conexao.close()
+    if inicio == 'desligar':
+        objeto = input("O que voce deseja desligar?(projetor/ lampada1/ lampada2/ arcondicionado/ sensorDePresenca/ sensorDeFumaca/ sensorDePorta/ sensorDeJanela)\n")        
+        tcp.send(str(objeto).encode())
+    
+    sleep(10)
+    frase = tcp.recv(1024)
+    print(frase)
+    
+print('Volte sempre.')
+tcp.close()
